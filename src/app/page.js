ddd38327d56f345057474688ee1946df43e55d85
morgansson <17188509+morgansson@users.noreply.github.com>
2024-03-09 01:32:45 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,147 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { Token } from '@uniswap/sdk-core';
+import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+
+const chainId = 8453; // Arbitrum chain ID
+const ERC20ABI = [
+    // Read-Only Functions
+    "function balanceOf(address owner) view returns (uint256)",
+    "function decimals() view returns (uint8)",
+    "function symbol() view returns (string)",
+
+    // Authenticated Functions
+    "function transfer(address to, uint amount) returns (bool)",
+
+    // Events
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+];
+
+const UniswapV3PriceSetter = () => {
+  const poolAddress = '0x9d489b739fa2f1987fc588809d213eaefb372f3b';
+  
+  const [targetPrice, setTargetPrice] = useState('200');
+  const [currentPrice, setCurrentPrice] = useState('');
+  const [currentToken0Reserve, setCurrentToken0Reserve] = useState('');
+  const [currentToken1Reserve, setCurrentToken1Reserve] = useState('');
+
+
+  useEffect(() => {
+    const fetchCurrentPriceAndSymbols = async () => {
+      const provider = new ethers.providers.JsonRpcProvider("https://autumn-few-shape.base-mainnet.quiknode.pro/4e7dc9cb673fd2393917d49ddeb31862959ffbc7");
+      const poolContract = new ethers.Contract(poolAddress, IUniswapV3PoolABI.abi, provider);
+
+      const token0Address = await poolContract.token0();
+      const token1Address = await poolContract.token1();
+
+      // Create ERC-20 contract instances for token0 and token1
+      const token0Contract = new ethers.Contract(token0Address, ERC20ABI, provider);
+      const token1Contract = new ethers.Contract(token1Address, ERC20ABI, provider);
+
+      // Fetch symbols and decimals for token0 and token1
+      const [token0Symbol, token1Symbol, token0Decimals, token1Decimals, token0Reserve, token1Reserve] = await Promise.all([
+        token0Contract.symbol(),
+        token1Contract.symbol(),
+        token0Contract.decimals(),
+        token1Contract.decimals(),
+        token0Contract.balanceOf(poolAddress),
+        token1Contract.balanceOf(poolAddress),
+      ]);
+
+      // Assuming 18 decimals for tokens, adjust accordingly
+      const token0 = new Token(chainId, token0Address, token0Decimals, token0Symbol);
+      const token1 = new Token(chainId, token1Address, token1Decimals, token1Symbol);
+
+      const adjustedPrice = token1Reserve / token0Reserve;
+
+      // Depending on your token order, adjust the price calculation
+      setCurrentPrice(`1 ${token0.symbol} = ${adjustedPrice.toString()} ${token1.symbol}`);
+      setCurrentToken0Reserve(token0Reserve.toString());
+      setCurrentToken1Reserve(token1Reserve.toString());
+
+    };
+
+    fetchCurrentPriceAndSymbols();
+  }, []);
+
+  function calculateSwapAmount(reserveA, reserveB, targetPrice) {
+    console.log("reserveA", reserveA)
+    console.log("reserveB", reserveB)
+    console.log("targetPrice", targetPrice)
+    //check if targetPrice is a number
+    if (isNaN(targetPrice)) {
+        targetPrice = Number(targetPrice);
+    }
+    if (targetPrice <= 0) {
+        targetPrice = 1;
+    }
+    
+    //convert to integer if float
+    targetPrice = Number(Number(targetPrice).toFixed(0));
+
+
+    // The target price is defined as amountB/amountA for tokenA to tokenB swap
+    const currentPrice = reserveB / reserveA;
+    if (targetPrice > currentPrice) {
+        // We need to increase the price, so we swap tokenA for tokenB
+        // Calculate the required reserve ratio to achieve the target price
+        const targetReserveB = BigInt(reserveA) * BigInt(targetPrice);
+        // Calculate how much tokenB we need to remove to achieve this ratio
+        const tokenBToSwap = ((BigInt(reserveB) - targetReserveB) / BigInt(2)) * BigInt(-1);
+        return { tokenAToSwap: 0, tokenBToSwap };
+    } else {
+        // We need to decrease the price, so we swap tokenB for tokenA
+        // Calculate the required reserve ratio to achieve the target price
+        const targetReserveA = BigInt(reserveB) / BigInt(targetPrice);
+
+        // Calculate how much tokenA we need to add to achieve this ratio
+        const tokenAToSwap = (targetReserveA - BigInt(reserveA)) / BigInt(2);
+        return { tokenAToSwap, tokenBToSwap: 0 };
+    }
+  }
+
+  let { tokenAToSwap, tokenBToSwap } = calculateSwapAmount(currentToken0Reserve, currentToken1Reserve, targetPrice);
+
+  try {
+      tokenAToSwap = ethers.utils.formatEther(String(tokenAToSwap));
+  } catch (e) {
+      console.log("Error converting swap amounts", e);
+  }
+
+  try {
+      tokenBToSwap = ethers.utils.formatEther(String(tokenBToSwap));
+  } catch (e) {
+      console.log("Error converting swap amounts", e);
+  }
+
+  if(!currentPrice) {
+    return <div class="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8 dark:text-neutral-200">Loading...</div>;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div class="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8">
+      <p class="label">Current Price: <span class="item">{currentPrice}</span></p>
+      <p class="label">Current Token0 Reserve: <span class="item">{currentToken0Reserve}</span></p>
+      <p class="label">Current Token1 Reserve: <span class="item">{currentToken1Reserve}</span></p>
+      <div class="mt-4">
+          <label class="block label">Target Price:</label>
+          <div className='flex items-center gap-2 label'>
+            <input type="text" value={targetPrice} onChange={(e) => setTargetPrice(e.target.value)} placeholder="Target Price" class="text-lg px-3 py-2 bg-gray-100 dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-md dark:text-gray-100"/>
+
+            <span>= 1 WETH</span> 
+          </div>
       </div>
+      {targetPrice && (
+          <div class="mt-4 bg-gray-100 dark:bg-neutral-900 p-4 rounded-lg">
+              <p class="label">WETH to SWAP: <span class="item">{tokenAToSwap}</span></p>
+              <p class="label">MEDIA to SWAP: <span class="item">{tokenBToSwap}</span></p>
+          </div>
+      )}
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
-}
+};
+
+export default UniswapV3PriceSetter;
