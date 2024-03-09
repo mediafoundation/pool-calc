127332d14b3d217796d165e9a8f0c556db210ec2
morgansson <17188509+morgansson@users.noreply.github.com>
2024-03-09 02:26:44 -0300

    few fixes and dynamic pool

--- src/app/page.js
+++ src/app/page.js
@@ -20,12 +20,13 @@ const ERC20ABI = [
 ];
 
 const UniswapV3PriceSetter = () => {
-  const poolAddress = '0x9d489b739fa2f1987fc588809d213eaefb372f3b';
-  
+  const [poolAddress, setPoolAddress] = useState('0x9d489b739fa2f1987fc588809d213eaefb372f3b');
   const [targetPrice, setTargetPrice] = useState('200');
   const [currentPrice, setCurrentPrice] = useState('');
   const [currentToken0Reserve, setCurrentToken0Reserve] = useState('');
   const [currentToken1Reserve, setCurrentToken1Reserve] = useState('');
+  const [token0, setToken0] = useState({});
+  const [token1, setToken1] = useState({});
 
 
   useEffect(() => {
@@ -54,6 +55,9 @@ const UniswapV3PriceSetter = () => {
       const token0 = new Token(chainId, token0Address, token0Decimals, token0Symbol);
       const token1 = new Token(chainId, token1Address, token1Decimals, token1Symbol);
 
+      setToken0(token0);
+      setToken1(token1);
+
       const adjustedPrice = token1Reserve / token0Reserve;
 
       // Depending on your token order, adjust the price calculation
@@ -64,40 +68,33 @@ const UniswapV3PriceSetter = () => {
     };
 
     fetchCurrentPriceAndSymbols();
-  }, []);
+  }, [poolAddress]);
 
   function calculateSwapAmount(reserveA, reserveB, targetPrice) {
-    console.log("reserveA", reserveA)
-    console.log("reserveB", reserveB)
-    console.log("targetPrice", targetPrice)
     //check if targetPrice is a number
     if (isNaN(targetPrice)) {
         targetPrice = Number(targetPrice);
     }
-    if (targetPrice <= 0) {
+    if (isNaN(targetPrice) || targetPrice <= 0) {
         targetPrice = 1;
     }
     
-    //convert to integer if float
-    targetPrice = Number(Number(targetPrice).toFixed(0));
-
-
     // The target price is defined as amountB/amountA for tokenA to tokenB swap
     const currentPrice = reserveB / reserveA;
     if (targetPrice > currentPrice) {
         // We need to increase the price, so we swap tokenA for tokenB
         // Calculate the required reserve ratio to achieve the target price
-        const targetReserveB = BigInt(reserveA) * BigInt(targetPrice);
+        const targetReserveB = reserveA * targetPrice;
         // Calculate how much tokenB we need to remove to achieve this ratio
-        const tokenBToSwap = ((BigInt(reserveB) - targetReserveB) / BigInt(2)) * BigInt(-1);
+        const tokenBToSwap = BigInt(((reserveB - targetReserveB) / 2) * -1);
         return { tokenAToSwap: 0, tokenBToSwap };
     } else {
         // We need to decrease the price, so we swap tokenB for tokenA
         // Calculate the required reserve ratio to achieve the target price
-        const targetReserveA = BigInt(reserveB) / BigInt(targetPrice);
+        const targetReserveA = reserveB / targetPrice;
 
         // Calculate how much tokenA we need to add to achieve this ratio
-        const tokenAToSwap = (targetReserveA - BigInt(reserveA)) / BigInt(2);
+        const tokenAToSwap = BigInt((targetReserveA - reserveA) / 2);
         return { tokenAToSwap, tokenBToSwap: 0 };
     }
   }
@@ -117,26 +114,30 @@ const UniswapV3PriceSetter = () => {
   }
 
   if(!currentPrice) {
-    return <div class="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8 dark:text-neutral-200">Loading...</div>;
+    return <div className="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8 dark:text-neutral-200">Loading...</div>;
   }
 
   return (
-    <div class="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8">
-      <p class="label">Current Price: <span class="item">{currentPrice}</span></p>
-      <p class="label">Current Token0 Reserve: <span class="item">{currentToken0Reserve}</span></p>
-      <p class="label">Current Token1 Reserve: <span class="item">{currentToken1Reserve}</span></p>
-      <div class="mt-4">
-          <label class="block label">Target Price:</label>
+    <div className="max-w-md mx-auto bg-white dark:bg-neutral-950 shadow-md rounded-lg p-4 m-8">
+      <div className="mb-4">
+        <label className="block label">Pool Address:</label>
+        <input type="text" value={poolAddress} onChange={(e) => setPoolAddress(e.target.value)} placeholder="Pool Address" className="text-lg px-3 py-2 bg-gray-100 dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-md dark:text-gray-100"/>
+      </div>
+      <p className="label">Current Price: <span className="item">{currentPrice}</span></p>
+      <p className="label">Current {token0.symbol} Reserve: <span className="item">{ethers.utils.formatUnits(currentToken0Reserve, token0.decimals)}</span></p>
+      <p className="label">Current {token1.symbol} Reserve: <span className="item">{ethers.utils.formatUnits(currentToken1Reserve, token1.decimals)}</span></p>
+      <div className="mt-4">
+          <label className="block label">Target Price:</label>
           <div className='flex items-center gap-2 label'>
-            <input type="text" value={targetPrice} onChange={(e) => setTargetPrice(e.target.value)} placeholder="Target Price" class="text-lg px-3 py-2 bg-gray-100 dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-md dark:text-gray-100"/>
+            <input type="text" value={targetPrice} onChange={(e) => setTargetPrice(e.target.value)} placeholder="Target Price" className="text-lg px-3 py-2 bg-gray-100 dark:bg-neutral-900 border border-gray-300 dark:border-neutral-700 rounded-md dark:text-gray-100"/>
 
             <span>= 1 WETH</span> 
           </div>
       </div>
       {targetPrice && (
-          <div class="mt-4 bg-gray-100 dark:bg-neutral-900 p-4 rounded-lg">
-              <p class="label">WETH to SWAP: <span class="item">{tokenAToSwap}</span></p>
-              <p class="label">MEDIA to SWAP: <span class="item">{tokenBToSwap}</span></p>
+          <div className="mt-4 bg-gray-100 dark:bg-neutral-900 p-4 rounded-lg">
+              <p className="label">{token0.symbol} to SWAP: <span className="item">{tokenAToSwap}</span></p>
+              <p className="label">{token1.symbol} to SWAP: <span className="item">{tokenBToSwap}</span></p>
           </div>
       )}
     </div>
